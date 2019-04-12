47b68da69eb8d39ecea5b402b17f4ff435769240
Benjamin Otto <benjamin.otto@style-systems.de>
2019-04-12 10:24:15 +0200

    private

--- src/LambdaApplicationResponse.kt
+++ src/LambdaApplicationResponse.kt
@@ -8,7 +8,7 @@ import io.ktor.server.engine.BaseApplicationResponse
 import io.ktor.util.toMap
 import kotlinx.coroutines.io.ByteChannel
 
-internal class LambdaApplicationResponse(call: ApplicationCall, val output: ByteChannel) :
+internal class LambdaApplicationResponse(call: ApplicationCall, private val output: ByteChannel) :
   BaseApplicationResponse(call) {
   private var statusCode: HttpStatusCode = HttpStatusCode.OK
   private val headersNames = ArrayList<String>()
